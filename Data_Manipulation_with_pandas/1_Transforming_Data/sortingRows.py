f8175e7d0776839000d76e4d10a016f41f3da3b0
Ehsanul Hoque <70466614+ehsanul18@users.noreply.github.com>
2020-12-03 19:38:58 +0600

    Create sortingRows.py

---
+++ Data_Manipulation_with_pandas/1_Transforming_Data/sortingRows.py
@@ -0,0 +1,47 @@
+# Sorting rows
+# Finding interesting bits of data in a DataFrame is often easier if you change the order of the rows. You can sort the rows by passing a column name to .sort_values().
+
+# In cases where rows have the same value (this is common if you sort on a categorical variable), you may wish to break the ties by sorting on another column. You can sort on multiple columns in this way by passing a list of column names.
+
+# Sort on …	Syntax
+# one column	df.sort_values("breed")
+# multiple columns	df.sort_values(["breed", "weight_kg"])
+# By combining .sort_values() with .head(), you can answer questions in the form, "What are the top cases where…?".
+
+# homelessness is available and pandas is loaded as pd.
+
+# Instructions 1/3
+# 35 XP
+# Sort homelessness by the number of homeless individuals, from smallest to largest, and save this as homelessness_ind.
+# Print the head of the sorted DataFrame.
+
+# Instructions 2/3
+# Sort homelessness by the number of homeless family_members in descending order, and save this as homelessness_fam.
+# Print the head of the sorted DataFrame.
+
+# Instructions 3/3
+# Sort homelessness first by region (ascending), and then by number of family members (descending). Save this as homelessness_reg_fam.
+# Print the head of the sorted DataFrame.
+
+
+# # Sort homelessness by individual (Instruction 1)
+# homelessness_ind = homelessness.sort_values(["individuals"], ascending=True)
+
+# # Print the top few rows
+# print(homelessness_ind.head())
+
+
+# # Sort homelessness by descending family members (Instruction 2)
+# homelessness_fam = homelessness.sort_values(['family_members'], ascending=False)
+
+# # Print the top few rows
+# print(homelessness_fam.head())
+
+
+# Sort homelessness by region, then descending family members (Instruction 3)
+homelessness_reg_fam = homelessness.sort_values(['region', 'family_members'], ascending=[True, False])
+
+# Print the top few rows
+print(homelessness_reg_fam.head())
+
+
